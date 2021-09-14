f7fc3519f803bc047c78e847753b663fee67b721
Gintaras Drukteinis <gintaras.drukteinis@rutronik.com>
2021-09-14 08:50:49 +0300

    First commit. Working example.

---
+++ main.c
@@ -0,0 +1,119 @@
+
+/******************************************************************************
+* File Name:   main.c
+*
+* Description: This is the source code for the Empty PSoC6 Application
+*              for ModusToolbox.
+*
+* Related Document: See README.md
+*
+*
+*******************************************************************************
+* (c) 2019-2021, Cypress Semiconductor Corporation. All rights reserved.
+*******************************************************************************
+* This software, including source code, documentation and related materials
+* ("Software"), is owned by Cypress Semiconductor Corporation or one of its
+* subsidiaries ("Cypress") and is protected by and subject to worldwide patent
+* protection (United States and foreign), United States copyright laws and
+* international treaty provisions. Therefore, you may use this Software only
+* as provided in the license agreement accompanying the software package from
+* which you obtained this Software ("EULA").
+*
+* If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
+* non-transferable license to copy, modify, and compile the Software source
+* code solely for use in connection with Cypress's integrated circuit products.
+* Any reproduction, modification, translation, compilation, or representation
+* of this Software except as specified above is prohibited without the express
+* written permission of Cypress.
+*
+* Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
+* EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
+* reserves the right to make changes to the Software without notice. Cypress
+* does not assume any liability arising out of the application or use of the
+* Software or any product or circuit described in the Software. Cypress does
+* not authorize its products for use in any products where a malfunction or
+* failure of the Cypress product may reasonably be expected to result in
+* significant property damage, injury or death ("High Risk Product"). By
+* including Cypress's product in a High Risk Product, the manufacturer of such
+* system or application assumes all risk of such use and in doing so agrees to
+* indemnify Cypress against all liability.
+*******************************************************************************/
+
+#include "cy_pdl.h"
+#include "cyhal.h"
+#include "cybsp.h"
+#include "cy_retarget_io.h"
+#include "EVE_target.h"
+#include "tft.h"
+
+void handle_error(void);
+
+int main(void)
+{
+	cy_rslt_t result;
+	volatile uint8_t display_delay = 0;
+
+
+    /* Initialize the device and board peripherals */
+    result = cybsp_init() ;
+    if (result != CY_RSLT_SUCCESS)
+    {
+    	handle_error();
+    }
+
+    /*Enable debug output via KitProg UART*/
+    result = cy_retarget_io_init( KITPROG_TX, KITPROG_RX, CY_RETARGET_IO_BAUDRATE);
+    if (result != CY_RSLT_SUCCESS)
+    {
+        handle_error();
+    }
+    printf("\x1b[2J\x1b[;H");
+    printf("Display Test \n\r");
+
+    /*Initialize LEDs*/
+    result = cyhal_gpio_init( LED1, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, CYBSP_LED_STATE_OFF);
+    if (result != CY_RSLT_SUCCESS)
+    {handle_error();}
+    result = cyhal_gpio_init( LED2, CYHAL_GPIO_DIR_OUTPUT, CYHAL_GPIO_DRIVE_STRONG, CYBSP_LED_STATE_OFF);
+    if (result != CY_RSLT_SUCCESS)
+    {handle_error();}
+
+    /*Enable interrupts*/
+    __enable_irq();
+
+    /*Initialize EVE*/
+    result = EVE_init_spi();
+    if (result != CY_RSLT_SUCCESS)
+    {handle_error();}
+    TFT_init();
+
+    printf("Display test has started...\n\r");
+    for (;;)
+    {
+    	CyDelay(5);
+
+    	TFT_touch();
+
+    	display_delay++;
+		if(display_delay > 3)
+		{
+			display_delay = 0;
+
+			TFT_display();
+
+			num_profile_b--;
+		}
+		num_profile_a++;
+    }
+}
+
+void handle_error(void)
+{
+     /* Disable all interrupts. */
+    __disable_irq();
+    cyhal_gpio_write(LED1, CYBSP_LED_STATE_OFF);
+    cyhal_gpio_write(LED2, CYBSP_LED_STATE_ON);
+    CY_ASSERT(0);
+}
+/* [] END OF FILE */
